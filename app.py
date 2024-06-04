667248156df59905a534b797d0beefcf0f2fe1a9
callistasava <90736677+callistasava@users.noreply.github.com>
2024-06-04 14:14:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ with tab2:
     st.write('')
     st.write('**Hasil Uji:**')
     col1, col2 = st.columns(2)
-    col1.image('cf-astro.png', use_column_width=True)
+    col1.image('cf-astro.png', width=350)
     col2.image('pie_astro.png', use_column_width=True)
     st.write('')
     st.write('Dari 74 ulasan yang termasuk dalam data uji, model ini memprediksi bahwa 2 ulasan memiliki sentimen negatif dan 72 ulasan memiliki sentimen positif. Deskripsi ini menggambarkan kemampuan model dalam mengkategorikan sentimen ulasan pengguna dengan cukup baik.')
